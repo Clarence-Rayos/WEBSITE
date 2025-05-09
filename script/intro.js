c22c5607c479d467c1577ea0ee2b722f339f155a
Clarence-Rayos <clarence.rayos05@gmail.com>
2025-05-09 16:58:45 +0800

    Add files via upload

---
+++ script/intro.js
@@ -0,0 +1,6 @@
+function goToPage() {
+    // Change this to your desired link or file
+    window.location.href = "http://127.0.0.1:5500/home.html";
+    // Or an external link:
+    // window.location.href = "https://example.com";
+  }
